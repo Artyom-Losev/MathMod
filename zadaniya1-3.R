0aa2e86c08d34487c66c17435b0a22fef8a4d3f7
Artyom-Losev <trilleriv@gmail.com>
2020-02-19 00:11:32 +0300

    Задание 1-3 правка третьего

--- zadaniya1-3.R
+++ zadaniya1-3.R
@@ -1,4 +1,6 @@
 zad1=list(summary(iris[,-5])[4,]);zad1
 zad2=apply(iris[1:150,-5],1,mean);zad2
-DNA=sample(c("A","T","G","C"), size=1000, replace=TRUE)
-summary(factor(DNA))/length(DNA)
+DNA=c("A","T","G","C")[runif(1000, 1, 5)];DNA
+AT=summary(factor(DNA))[-c(2,3)]
+ratio=AT/length(DNA)
+dna_at=c(AT,ratio);dna_at
